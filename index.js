429f82480217aff9643d0a99239334d3afc97d8e
ella gortsinskaja <ella.gortsinskaja@ivkhk.ee>
2021-11-25 08:36:59 +0200

    second

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 
+const path = require('path');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'views', 'about'))
+})
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
-    console.log(`server is running on ${PORT}`)
+    console.log(`Server is running on ${PORT}`)
 })
